41509ea8a153ef903740a7501c05bf44705af148
Matheus Henrique <matthpmoreira@gmail.com>
2024-08-21 02:11:03 -0300

    feat: make day 1 work with new start up script

--- 2018/src/days/1.lua
+++ 2018/src/days/1.lua
@@ -1,23 +1,23 @@
-local input = ...
+local function main(input)
+	local frequency = 0
+	local repetitions = { [frequency] = 1 }
+	local first_repetition, calibration
 
-local frequency = 0
-local repetitions = { [frequency] = 1 }
-local first_repetition, calibration
-
-while not first_repetition do
-	input:seek("set")
-
-	for line in input:lines() do
-		frequency = frequency + tonumber(line)
-		repetitions[frequency] = repetitions[frequency] or 0
-		repetitions[frequency] = repetitions[frequency] + 1
-	
-		if repetitions[frequency] == 2 then
-			first_repetition = first_repetition or frequency
+	while not first_repetition do
+		for line in string.gmatch(input, "[^\r\n]+") do
+			frequency = frequency + tonumber(line)
+			repetitions[frequency] = repetitions[frequency] or 0
+			repetitions[frequency] = repetitions[frequency] + 1
+		
+			if repetitions[frequency] == 2 then
+				first_repetition = first_repetition or frequency
+			end
 		end
+
+		calibration = calibration or frequency
 	end
 
-	calibration = calibration or frequency
+	return calibration, first_repetition
 end
 
-return calibration, first_repetition
+return main
