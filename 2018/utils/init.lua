4802d70e5ebecd9293854a10550f3146a0617e66
Matheus Henrique <matthpmoreira@gmail.com>
2024-08-22 06:40:21 -0300

    refactor: keep package.path
    I'm still a little confused with Lua's module system, but I've decided it's better to not modify package.path and embrace the structure of my project. As an application, there's no need for it to prepend itself to module names when requiring them.

---
+++ 2018/utils/init.lua
@@ -0,0 +1,13 @@
+local filter = require("utils/filter")
+local find = require("utils/find")
+local iterateLines = require("utils/iterateLines")
+local map = require("utils/map")
+local split = require("utils/split")
+
+return {
+    filter = filter,
+    find = find,
+    iterateLines = iterateLines,
+    map = map,
+    split = split,
+}
