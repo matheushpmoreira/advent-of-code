103cecb15aa049a8587538e5746649e10f5a4da7
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-18 13:32:38 -0300

    Refactor fetch-input script

--- fetch-input.sh
+++ fetch-input.sh
@@ -1,34 +1,30 @@
 #!/usr/bin/env sh
 
-# TODO:
-#   - functionalize operations
-#   - pre-cast arguments to numbers
-#   - add option to clear cache
+set -e
+cd "$(dirname "$0")"
+
+error() {
+    printf "\033[1;37;41m$1\033[0m\n" 1>&2
+    exit 1
+}
 
 if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "usage: ./fetch-input <year> <day>"
-    return 1
+    error "usage: ./fetch-input <year> <day>"
 fi
 
-cd "$(dirname "$0")" || exit
-
-year=$1
-day=$2
-
-if [ -e "cache/$year/$day" ]; then
-    cat "cache/$year/$day"
-    return
+if [ ! -e session ]; then
+    error "error: missing session token"
 fi
 
-session=$(cat session)
-url="https://adventofcode.com/$year/day/$day/input"
-response=$(curl -sb "session=$session" "$url")
+YEAR=$1
+DAY=$2
+SESSION=$(cat session)
+mkdir -p "cache/$YEAR"
 
-echo "$response"
-
-if [ "$response" = "404 Not Found" ]; then
-    return 1
-else
-    mkdir -p "cache/$year"
-    echo "$response" > "cache/$year/$day"
+if [ ! -e "cache/$YEAR/$DAY" ]; then
+    URL="https://adventofcode.com/$YEAR/day/$DAY/input"
+    INPUT=$(curl -sSfb "session=$SESSION" "$URL")
+    printf "$INPUT\n" > "cache/$YEAR/$DAY"
 fi
+
+cat "cache/$YEAR/$DAY"
