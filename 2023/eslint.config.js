d5e35129bc4138ae9236857c25f54dbae614b935
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-18 14:15:45 -0300

    Initial commit for 2023

---
+++ 2023/eslint.config.js
@@ -0,0 +1,10 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    { languageOptions: { globals: { ...globals.node } } },
+    { ignores: ["build"] },
+];
