1bb7c23933b615cfdca891202b13ba9ffb65f9f3
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-19 16:09:12 -0300

    Ignore possibility of null

--- 2023/utils/stringx.ts
+++ 2023/utils/stringx.ts
@@ -1,4 +1,4 @@
 export function splitLines(multiline: string) {
     // Consistent splitting, regardless of carriage return or trailing newline
-    return multiline.match(/[^\r\n]+/g);
+    return multiline.match(/[^\r\n]+/g)!;
 }
