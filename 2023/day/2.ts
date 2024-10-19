ecacf884c49ea10c8ed0c16ee7c8cb79151a7319
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-19 15:03:23 -0300

    Conclude 2023/2

--- 2023/day/2.ts
+++ 2023/day/2.ts
@@ -1,26 +1,43 @@
+class Game {
+    id: number;
+    red: number;
+    blue: number;
+    green: number;
+    power: number;
+
+    constructor(str: string) {
+        this.id = Number(str.match(/Game (\d+):/)![1]);
+        this.red = findGreatestDieCount(str, "red");
+        this.blue = findGreatestDieCount(str, "blue");
+        this.green = findGreatestDieCount(str, "green");
+        this.power = this.red * this.blue * this.green;
+    }
+}
+
+function findGreatestDieCount(str: string, color: string) {
+    return Array.from(str.matchAll(new RegExp(`(\\d+) ${color}`, "g")), match => Number(match[1])).reduce((max, cur) =>
+        max > cur ? max : cur
+    );
+}
+
 export function solve(input: string) {
     const maxDie = {
-	red: 12,
-	green: 13,
-	blue: 14
+        red: 12,
+        green: 13,
+        blue: 14,
     } as const;
 
-    const part1 = input.split("\n").filter(Boolean)
-	.map(game => ({
-	    id: Number(game.match(/Game (\d+):/)![1]),
-	    red: Array.from(game.matchAll(/(\d+) red/g), match => Number(match[1])).reduce((max, cur) => max > cur ? max : cur),
-	    blue: Array.from(game.matchAll(/(\d+) blue/g), match => Number(match[1])).reduce((max, cur) => max > cur ? max : cur),
-	    green: Array.from(game.matchAll(/(\d+) green/g), match => Number(match[1])).reduce((max, cur) => max > cur ? max : cur)
-	}))
-	.reduce((sum, game) => {
-	    const hasEnoughDie = (
-		game.red <= maxDie.red &&
-		game.blue <= maxDie.blue &&
-		game.green <= maxDie.green
-	    )
+    const games = input
+        .split("\n")
+        .filter(Boolean)
+        .map(line => new Game(line));
 
-	    return sum + game.id * Number(hasEnoughDie)
-	}, 0)
+    const part1 = games.reduce((sum, game) => {
+        const hasEnoughDie = game.red <= maxDie.red && game.blue <= maxDie.blue && game.green <= maxDie.green;
+        return sum + game.id * Number(hasEnoughDie);
+    }, 0);
 
-	return {part1}
+    const part2 = games.reduce((sum, game) => sum + game.power, 0);
+
+    return { part1, part2 };
 }
