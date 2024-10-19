f34a4c1a83a34e555604f5135927b5498f7e15b0
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-19 16:54:08 -0300

    Fix possibility of inputs having printf directives

--- fetch-input.sh
+++ fetch-input.sh
@@ -4,7 +4,7 @@ set -e
 cd "$(dirname "$0")"
 
 error() {
-    printf "\033[1;37;41m$1\033[0m\n" 1>&2
+    printf "%s" "\033[1;37;41m$1\033[0m\n" 1>&2
     exit 1
 }
 
@@ -24,7 +24,7 @@ mkdir -p "cache/$YEAR"
 if [ ! -e "cache/$YEAR/$DAY" ]; then
     URL="https://adventofcode.com/$YEAR/day/$DAY/input"
     INPUT=$(curl -sSfb "session=$SESSION" "$URL")
-    printf "$INPUT" > "cache/$YEAR/$DAY"
+    printf "%s" "$INPUT" > "cache/$YEAR/$DAY"
 fi
 
 cat "cache/$YEAR/$DAY"
