1db86f1288501cec8c80f3446995510db810d9b1
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-21 09:09:56 -0300

    Refactor 2023/2

--- 2023/days/2.ts
+++ 2023/days/2.ts
@@ -5,35 +5,36 @@ class Game {
     green: number;
     power: number;
 
-    constructor(str: string) {
-        this.id = Number(str.match(/Game (\d+):/)![1]);
-        this.red = findGreatestDieCount(str, "red");
-        this.blue = findGreatestDieCount(str, "blue");
-        this.green = findGreatestDieCount(str, "green");
+    constructor(set: string) {
+        this.id = Number(set.match(/Game (\d+):/)![1]);
+        this.red = findGreatestDieCount(set, "red");
+        this.blue = findGreatestDieCount(set, "blue");
+        this.green = findGreatestDieCount(set, "green");
         this.power = this.red * this.blue * this.green;
     }
 }
 
-function findGreatestDieCount(str: string, color: string) {
-    return Array.from(str.matchAll(new RegExp(`(\\d+) ${color}`, "g")), match => Number(match[1])).reduce((max, cur) =>
-        max > cur ? max : cur
-    );
+const maxDie = {
+    red: 12,
+    green: 13,
+    blue: 14,
+} as const;
+
+const hasEnoughDie = (game: Game) => game.red <= maxDie.red && game.blue <= maxDie.blue && game.green <= maxDie.green;
+const maxAmount = (max: number, num: number) => (max > num ? max : num);
+
+function findGreatestDieCount(set: string, color: string) {
+    const regex = new RegExp(`(\\d+) ${color}`, "g");
+    return set
+        .matchAll(regex)
+        .toArray()
+        .map(match => Number(match[1]))
+        .reduce(maxAmount);
 }
 
-export function solve(input: string) {
-    const maxDie = {
-        red: 12,
-        green: 13,
-        blue: 14,
-    } as const;
-
-    const games = input.split("\n").map(line => new Game(line));
-
-    const part1 = games.reduce((sum, game) => {
-        const hasEnoughDie = game.red <= maxDie.red && game.blue <= maxDie.blue && game.green <= maxDie.green;
-        return sum + game.id * Number(hasEnoughDie);
-    }, 0);
-
+export function solve(record: string) {
+    const games = record.split("\n").map(set => new Game(set));
+    const part1 = games.reduce((sum, game) => sum + game.id * Number(hasEnoughDie(game)), 0);
     const part2 = games.reduce((sum, game) => sum + game.power, 0);
 
     return { part1, part2 };
