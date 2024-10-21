5865c4cea3fafec4c8985ce35ec3e8b075dee5d9
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-21 08:52:22 -0300

    Refactor 2023/1

--- 2023/days/1.ts
+++ 2023/days/1.ts
@@ -1,36 +1,36 @@
-export function solve(input: string) {
-    const part1 = processInput(input, /\d/g);
-    const part2 = processInput(input, /(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g);
+const spelledDigits: { [key: string]: number } = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+} as const;
+
+const spelledToDigit = (num: string) => spelledDigits[num] ?? num;
+const sumValues = (sum: number, val: number) => sum + val;
+
+export function solve(document: string) {
+    const part1 = sumValuesIn(document, /\d/g);
+    const part2 = sumValuesIn(document, /(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g);
 
     return { part1, part2 };
 }
 
-function processInput(input: string, pattern: RegExp) {
-    const spelledDigits: { [key: string]: number } = {
-        one: 1,
-        two: 2,
-        three: 3,
-        four: 4,
-        five: 5,
-        six: 6,
-        seven: 7,
-        eight: 8,
-        nine: 9,
-    };
-
-    return input
+function sumValuesIn(document: string, pattern: RegExp) {
+    return document
         .split("\n")
         .map(line => {
-            const digits = Array.from(line.matchAll(pattern))
-                .flat()
-                .filter(Boolean)
-                .map(num => spelledDigits[num] ?? Number(num));
+            const digits = line.matchAll(pattern).toArray().flat().filter(Boolean).map(spelledToDigit);
 
             const first = digits.at(0);
             const last = digits.at(-1);
-            const composite = Number(`${first}${last}`);
+            const value = Number(`${first}${last}`);
 
-            return composite;
+            return value;
         })
-        .reduce((acc, num) => acc + num, 0);
+        .reduce(sumValues, 0);
 }
