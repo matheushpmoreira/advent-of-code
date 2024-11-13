2de0ba7cae6d66368c91c69a9c34c05c4f7c1361
Matheus Henrique <matthpmoreira@gmail.com>
2024-11-12 23:31:49 -0300

    Temporary solution for handling example inputs

--- 2023/index.ts
+++ 2023/index.ts
@@ -1,6 +1,8 @@
 import { execSync } from "node:child_process";
+import { readFileSync } from "node:fs";
 
 const day = Number(process.argv[2]);
+const example = Boolean(process.argv[3]);
 
 if (Number.isNaN(day) || day < 1 || day > 25) {
     throw new Error("'day' argument must be a number between 1 and 25");
@@ -8,9 +10,15 @@ if (Number.isNaN(day) || day < 1 || day > 25) {
 
 const scriptDir = import.meta.dirname.match(/(^.*\/)2023/)!.at(1);
 const scriptPath = scriptDir + "fetch-input.sh";
-const input = execSync(`env sh ${scriptPath} 2023 ${day}`, { encoding: "utf8" })
+let input = execSync(`env sh ${scriptPath} 2023 ${day}`, { encoding: "utf8" })
     .replace(/\r?\n/g, "\n") // Normalize newline
     .replace(/\n+$/, ""); // Remove trailing newline
+
+if (example) {
+    input = readFileSync(scriptDir + "2023/example", { encoding: "utf8" })
+        .replace(/\r?\n/g, "\n") // Normalize newline
+        .replace(/\n+$/, ""); // Remove trailing newline
+}
 
 const { solve } = await import(`#days/${day}`);
 const { part1, part2 } = solve(input);
