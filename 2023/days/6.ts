714d9781251233768c905f6bd98ee1b50833e57d
Matheus Henrique <matthpmoreira@gmail.com>
2024-11-27 20:05:57 -0300

    Remove 'parse' from 2023/6

--- 2023/days/6.ts
+++ 2023/days/6.ts
@@ -1,27 +1,26 @@
-import { parse } from "utils/stringx.js";
 import { zip } from "utils/arrayx.js";
 
 export function solve(sheet: Input): Solution {
-    const kernedRace = [sheet.replace(/ /g, "")[parse](/\d+/g).map(Number)];
-    const races = sheet
-        .split("\n")
-        .map(line => line[parse](/\d+/g).map(Number))
-        [zip]();
+    const kernedRace = [sheet.replace(/ /g, "").match(/\d+/g)!.map(Number)];
+    const races = sheet.split("\n").map(line => line.match(/\d+/g)!.map(Number));
 
-    const part1 = calcWaysToWin(races);
+    // When zipping the races at the variable definition, Prettier breaks it
+    // into a new line, which causes ESLint to complain.
+    const part1 = calcWaysToWin(races[zip]());
     const part2 = calcWaysToWin(kernedRace);
 
     return { part1, part2 };
 }
 
-function calcWaysToWin(races: number[][]) {
+function calcWaysToWin(races: number[][]): number {
     return races
         .map(({ 0: time, 1: distance }) => bhaskara(1, -time, distance))
         .map(({ 0: min, 1: max }) => Math.ceil(max) - Math.floor(min) - 1)
         .reduce((acc, ways) => acc * ways);
 }
 
-function bhaskara(a: number, b: number, c: number) {
+function bhaskara(a: number, b: number, c: number): [number, number] {
     const sqrt = Math.sqrt(b ** 2 - 4 * a * c);
-    return [((-b + sqrt) / 2) * a, ((-b - sqrt) / 2) * a].sort((a, b) => a - b);
+    const xs = [((-b + sqrt) / 2) * a, ((-b - sqrt) / 2) * a] satisfies [number, number];
+    return xs.sort((a, b) => a - b);
 }
