a43c151ce1cf06cbbc11de823c9b33b0f9a331be
Matheus Henrique <matthpmoreira@gmail.com>
2024-11-28 17:07:12 -0300

    Remove aliases from pacakge.json

--- 2023/utils/convertToEnum.ts
+++ 2023/utils/convertToEnum.ts
@@ -1,5 +1,4 @@
 export function convertToEnum<E extends Record<string, string | number>>(enumObj: E, value: string): E[keyof E] {
-    // const key = Object.keys(enumObj).find(key => enumObj[key as keyof E] === value);
     const key: keyof E | undefined = Object.keys(enumObj).find(key => enumObj[key] === value);
 
     if (key == null) {
