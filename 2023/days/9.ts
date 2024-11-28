7e9a1fc33057bcd5f3457f738a936d44164ea31d
Matheus Henrique <matthpmoreira@gmail.com>
2024-11-28 14:32:29 -0300

    Refactor 2023/9

--- 2023/days/9.ts
+++ 2023/days/9.ts
@@ -1,41 +1,41 @@
-import { parse } from "utils/stringx.js";
-
 type Sequence = number[];
-type SumEdgesFunction = (sum: number, seq: Sequence) => number;
+type Derivation = Sequence[];
 
 export function solve(report: Input): Solution {
+    // Each derivation has the order of its sequences reversed, because they
+    // have to be read from bottom to top to calculate the extrapolated values
+    // on the left side, but it doesn't 1TzkVCrXer on the right side.
     const derivations = report
         .split("\n")
-        .map(line => line[parse](/-?\d+/g).map(Number) as Sequence)
-        .map(history => [history].concat(createDifferences(history)).reverse());
+        .map(line => line.split(" ").map(Number))
+        .map(sequence => createDerivation(sequence).reverse());
 
-    const part1 = predictAndSum(derivations, "next");
-    const part2 = predictAndSum(derivations, "previous");
+    const part1 = derivations
+        .map(deriv => deriv.reduce((acc, seq) => acc + Number(seq.at(-1)), 0))
+        .reduce((acc, num) => acc + num, 0);
+
+    const part2 = derivations
+        .map(deriv => deriv.reduce((acc, seq) => seq[0] - acc, 0))
+        .reduce((acc, num) => acc + num, 0);
 
     return { part1, part2 };
 }
 
-function createDifferences(sequence: Sequence): Sequence[] {
-    if (sequence.every(x => x === 0)) {
-        return [];
+function createDerivation(sequence: Sequence): Derivation {
+    const derivation = [];
+
+    while (!sequence.every(x => x === 0)) {
+        const differences = [];
+
+        for (let i = 0; i < sequence.length - 1; i++) {
+            const curr = sequence[i];
+            const next = sequence[i + 1];
+            differences.push(next - curr);
+        }
+
+        derivation.push(sequence);
+        sequence = differences;
     }
 
-    const differences = [] as Sequence;
-
-    for (let i = 0; i < sequence.length - 1; i++) {
-        const curr = sequence[i];
-        const next = sequence[i + 1];
-        differences.push(next - curr);
-    }
-
-    return [differences].concat(createDifferences(differences));
-}
-
-function predictAndSum(derivations: Sequence[][], edgeFlag: "previous" | "next") {
-    const sumEdges = (
-        edgeFlag === "previous" ?
-            (sum, seq) => seq[0] - sum
-        :   (sum, seq) => sum + seq.at(-1)!) as SumEdgesFunction;
-
-    return derivations.map(differences => differences.reduce(sumEdges, 0)).reduce((sum, num) => sum + num, 0);
+    return derivation;
 }
