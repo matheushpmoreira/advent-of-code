9591287eba7fde04736b80d16600a271b720ec6e
Matheus Henrique <matthpmoreira@gmail.com>
2024-12-12 22:17:50 -0300

    feat(2024): move day validation to separate method

--- 2024/src/main/java/me/arzcbnh/adventofcode/AdventOfCode.java
+++ 2024/src/main/java/me/arzcbnh/adventofcode/AdventOfCode.java
@@ -7,15 +7,7 @@ public class AdventOfCode {
     private static InputService inputService = new InputService();
 
     public static void main(String[] args) {
-        // Normalize day number
-        var day = args[0];
-        var num = Integer.parseInt(day);
-        day = Integer.toString(num);
-
-        if (num < 1 || num > 25) {
-            throw new IllegalArgumentException("Day must be between 1 and 25");
-        }
-
+        var day = normalizeDay(args[0]);
         var input = inputService.getInput(day);
         var dayInstance = DayFactory.createDay(day, input);
         var answer = dayInstance.solve();
@@ -23,5 +15,19 @@ public class AdventOfCode {
         System.out.println("--- Advent of Code 2024 ---");
         System.out.println("Part 1: " + answer.part1());
         System.out.println("Part 2: " + answer.part2());
+    }
+
+    private static String normalizeDay(String day) {
+        try {
+            var num = Integer.parseInt(day);
+
+            if (num < 1 || num > 25) {
+                throw new IllegalArgumentException("Day must be a number between 1 and 25");
+            }
+
+            return Integer.toString(num);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Day must be a number between 1 and 25", e);
+        }
     }
 }
