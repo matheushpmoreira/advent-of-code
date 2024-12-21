26e6d8e2b3e7b0f77b18ec8e6f7c9d0d71229fa5
Matheus Henrique <matthpmoreira@gmail.com>
2024-12-21 10:20:14 -0300

    refactor(2024): add day to CLI output and type some variables

--- 2024/src/main/java/me/arzcbnh/adventofcode/AdventOfCode.java
+++ 2024/src/main/java/me/arzcbnh/adventofcode/AdventOfCode.java
@@ -8,12 +8,12 @@ public class AdventOfCode {
     private static InputService inputService = new InputService();
 
     public static void main(String[] args) {
-        var day = Normalizer.day(args[0]);
-        var input = inputService.getInput(day);
+        String day = Normalizer.day(args[0]);
+        String input = inputService.getInput(day);
         var dayInstance = Day.getSolution(day, input);
         var answer = dayInstance.solve();
 
-        System.out.println("--- Advent of Code 2024 ---");
+        System.out.println("--- Advent of Code 2024, day " + day + " ---");
         System.out.println("Part 1: " + answer.part1());
         System.out.println("Part 2: " + answer.part2());
     }
