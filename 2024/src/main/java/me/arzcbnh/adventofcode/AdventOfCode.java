fc367df13b91142ad70a933cd889905d75f870f8
Matheus Henrique <matthpmoreira@gmail.com>
2024-12-29 16:59:01 -0300

    refactor(2024): move Day resolution to main class

--- 2024/src/main/java/me/arzcbnh/adventofcode/AdventOfCode.java
+++ 2024/src/main/java/me/arzcbnh/adventofcode/AdventOfCode.java
@@ -1,5 +1,6 @@
 package me.Matt.adventofcode;
 
+import me.Matt.adventofcode.days.Answer;
 import me.Matt.adventofcode.days.Day;
 import me.Matt.adventofcode.utils.InputService;
 import me.Matt.adventofcode.utils.Normalizer;
@@ -7,6 +8,8 @@ import me.Matt.adventofcode.utils.Normalizer;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.stream.Collectors;
 
 public class AdventOfCode {
@@ -29,12 +32,30 @@ public class AdventOfCode {
             input = inputService.getInput(day);
         }
 
-//        String input = inputService.getInput(day);
-        var dayInstance = Day.getSolution(day);
-        var answer = dayInstance.solve(input);
+        var answer = solveDay(Integer.parseInt(day), input);
 
         System.out.println("--- Advent of Code 2024, day " + day + " ---");
         System.out.println("Part 1: " + answer.part1());
         System.out.println("Part 2: " + answer.part2());
+    }
+
+    public static Answer solveDay(int day, String input) {
+        if (day < 1 || day > 25) {
+            throw new IllegalArgumentException("Day must be a number between 1 and 25");
+        }
+
+        try {
+            String className = "me.Matt.adventofcode.days.Day" + day;
+            Class<?> dayClass = Class.forName(className);
+            Constructor<?> constructor = dayClass.getConstructor();
+            Day instance = (Day) constructor.newInstance();
+            return instance.solve(input);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalArgumentException("Day " + day + " not implemented", e);
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException("Day " + day + " not properly implemented", e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- 2024/src/main/java/me/arzcbnh/adventofcode/days/Day.java
+++ 2024/src/main/java/me/arzcbnh/adventofcode/days/Day.java
@@ -1,33 +1,5 @@
 package me.Matt.adventofcode.days;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import lombok.experimental.StandardException;
-import me.Matt.adventofcode.utils.Normalizer;
-
 public abstract class Day {
-    public static Day getSolution(String day) {
-        return getSolution(Integer.parseInt(Normalizer.day(day)));
-    }
-
-    public static Day getSolution(Number day) {
-        try {
-            String className = "me.Matt.adventofcode.days.Day" + day;
-            Class<?> dayClass = Class.forName(className);
-            Constructor<?> constructor = dayClass.getConstructor();
-            Day instance = (Day) constructor.newInstance();
-            return instance;
-        } catch (ClassNotFoundException e) {
-            throw new IllegalArgumentException("Day " + day + " not implemented", e);
-        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException e) {
-            throw new DayFactoryException("Day " + day + " not properly implemented", e);
-        } catch (InvocationTargetException e) {
-            throw new DayFactoryException(e);
-        }
-    }
-
     public abstract Answer solve(String input);
-
-    @StandardException
-    public static class DayFactoryException extends RuntimeException {}
 }
