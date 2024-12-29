c6e93f637fd0fc929ba3e76aeea527d474d666b8
Matheus Henrique <matthpmoreira@gmail.com>
2024-12-29 16:38:00 -0300

    feat(2024): create temporary example input reader
    It's messy, but it's temporary!

--- 2024/src/main/java/me/arzcbnh/adventofcode/AdventOfCode.java
+++ 2024/src/main/java/me/arzcbnh/adventofcode/AdventOfCode.java
@@ -4,12 +4,32 @@ import me.Matt.adventofcode.days.Day;
 import me.Matt.adventofcode.utils.InputService;
 import me.Matt.adventofcode.utils.Normalizer;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
 public class AdventOfCode {
     private static InputService inputService = new InputService();
 
     public static void main(String[] args) {
         String day = Normalizer.day(args[0]);
-        String input = inputService.getInput(day);
+        String input;
+
+        try {
+            var example = args[1];
+            ClassLoader loader = AdventOfCode.class.getClassLoader();
+            try (InputStream stream = loader.getResourceAsStream("example")) {
+                var reader = new BufferedReader(new InputStreamReader(stream));
+                input = reader.lines().collect(Collectors.joining("\n"));
+            } catch (Exception e) {
+                throw e;
+            }
+        } catch (Exception e) {
+            input = inputService.getInput(day);
+        }
+
+//        String input = inputService.getInput(day);
         var dayInstance = Day.getSolution(day, input);
         var answer = dayInstance.solve();
 
