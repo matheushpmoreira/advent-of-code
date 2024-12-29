1045fc773d76e1af96c2d368529feba25f18cb89
Matheus Henrique <matthpmoreira@gmail.com>
2024-12-29 13:53:48 -0300

    feat(2023): start using parseArgs for CLI
    I finally looked into the "getopts" version of Node.

--- 2023/src/cli.ts
+++ 2023/src/cli.ts
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 
 import { solveDay } from "#root/index.js";
+import { parseArgs } from "node:util";
 
-const day = Number(process.argv[2]);
-const useExample = Boolean(process.argv[3]);
+const { values, positionals } = parseArgs({
+    args: process.argv.slice(2),
+    allowPositionals: true,
+    options: {
+        example: {
+            type: "boolean",
+            short: "e",
+        },
+    },
+});
+
+const day = Number(positionals[0]);
+const useExample = values.example;
 
 const { part1, part2 } = await solveDay(day, useExample);
 
