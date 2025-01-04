adfd2ec5d3387854ed07667862c290307129263b
Matheus Henrique <matthpmoreira@gmail.com>
2025-01-04 15:27:53 -0300

    refactor(2024): delete script `run`, add option `--prompt`, use IO streams
    By having the program itself deal with prompting for a day, I can get rid of the shell's command `read` and move every "mode" of the script to my IDE:
    
    - `dev` and its derivatives wrap prompts around a Maven goal, but now the prompts are done by the program itself.
    - `*` executes the program JAR, so it also has prompting.
    - `build` and `format` are just Maven goals.
    
    Since I had to wrap `System.in` in a `BufferedReader` to make the prompt, I took the plunge and upgraded both standard streams.

--- 2024/src/main/java/me/arzcbnh/adventofcode/AdventOfCode.java
+++ 2024/src/main/java/me/arzcbnh/adventofcode/AdventOfCode.java
@@ -1,5 +1,6 @@
 package me.Matt.adventofcode;
 
+import java.io.*;
 import me.Matt.adventofcode.day.SolverFactory;
 import me.Matt.adventofcode.input.InputProvider;
 import me.Matt.adventofcode.input.InputServiceFactory;
@@ -8,34 +9,49 @@ import org.apache.commons.cli.*;
 public class AdventOfCode {
     private static final class CLI {
         int day;
-        boolean useExample;
+        boolean example;
+        boolean prompt;
     }
 
     private static final InputProvider inputProvider = InputServiceFactory.createInputService();
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final PrintWriter writer = new PrintWriter(System.out, true);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         var cli = parseArgs(args);
 
-        String input = cli.useExample ? inputProvider.getExample() : inputProvider.getInput(cli.day);
+        if (cli.prompt) {
+            writer.print("Day: ");
+            writer.flush();
+            cli.day = Integer.parseInt(reader.readLine());
+        }
+
+        String input = cli.example ? inputProvider.getExample() : inputProvider.getInput(cli.day);
 
         var solverInstance = SolverFactory.createDay(cli.day);
         var answer = solverInstance.solve(input);
 
-        System.out.println("--- Advent of Code 2024, day " + cli.day + " ---");
-        System.out.println("Part 1: " + answer.part1());
-        System.out.println("Part 2: " + answer.part2());
+        writer.println("--- Advent of Code 2024, day " + cli.day + " ---");
+        writer.println("Part 1: " + answer.part1());
+        writer.println("Part 2: " + answer.part2());
     }
 
     private static CLI parseArgs(String[] args) {
         var options = new Options()
-                .addOption("e", "example", false, "use file 'example' in the current working directory as input");
+                .addOption("e", "example", false, "use file 'example' in the current working directory as input")
+                .addOption("p", "prompt", false, "prompt for a day");
 
         try {
             CommandLine cmd = new DefaultParser().parse(options, args);
 
             var cli = new CLI();
-            cli.day = Integer.parseInt(cmd.getArgs()[0]);
-            cli.useExample = cmd.hasOption("e");
+            cli.example = cmd.hasOption("e");
+
+            if (cmd.hasOption("p")) {
+                cli.prompt = cmd.hasOption("p");
+            } else {
+                cli.day = Integer.parseInt(cmd.getArgs()[0]);
+            }
 
             return cli;
         } catch (ParseException e) {
